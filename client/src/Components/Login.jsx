b7e1d3339e6c6ca814556d93cd8844c71e67e381
Alyamama6 <alyadvweb@gmail.com>
2026-05-15 21:47:06 +0400

    Update Login.jsx

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 
   const onSubmit = async (formData) => {
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://bloom-hair-final.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
